ea67eb015941aeb4f6d8e1553ddcd370e378f270
josuevanier <150824779+josuevanier@users.noreply.github.com>
2025-03-12 21:12:53 -0400

    Added Proper imports and change the name of one of the variables in scene builder

--- src/main/java/edu/vanier/template/controllers/StartPageController.java
+++ src/main/java/edu/vanier/template/controllers/StartPageController.java
@@ -1,4 +1,13 @@
 package edu.vanier.template.controllers;
 
+import javafx.fxml.FXML;
+
+import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+
 public class StartPageController {
+    @FXML
+    private Label labelTitle;
+    @FXML
+    private Button buttonStart;
 }
