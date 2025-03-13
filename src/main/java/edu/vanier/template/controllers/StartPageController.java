698ffcdd74ccc40a471dd9c0a3757404303a8963
josuevanier <150824779+josuevanier@users.noreply.github.com>
2025-03-12 21:24:38 -0400

    Added Documentation and comment

--- src/main/java/edu/vanier/template/controllers/StartPageController.java
+++ src/main/java/edu/vanier/template/controllers/StartPageController.java
@@ -5,9 +5,25 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.Button;
 
+/**
+ * FXML controller  for the start page.
+ *
+ * @author Joseph Josue Forestal
+ */
 public class StartPageController {
     @FXML
     private Label labelTitle;
     @FXML
     private Button buttonStart;
+
+    @FXML
+    public void initialize() {
+        buttonStart.setOnAction(event -> handleStartButton());
+    }
+    // I'm not sure but here its probably going to be with the scene controller to change scene
+    public  void handleStartButton(){
+        // we add logic here
+    }
+
 }
+
