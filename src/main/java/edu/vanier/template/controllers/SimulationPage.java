fdc01537a4fbe9e1663e901d1556f5e5b48673ee
josuevanier <150824779+josuevanier@users.noreply.github.com>
2025-03-15 09:35:10 -0400

    Created the most general simulation page

---
+++ src/main/java/edu/vanier/template/controllers/SimulationPage.java
@@ -0,0 +1,17 @@
+package edu.vanier.template.controllers;
+
+import javafx.scene.SubScene;
+import javafx.scene.layout.HBox;
+
+import java.awt.*;
+
+/**
+ *This is the most general page for a template , (we can add a method that put the pre assets based on the choice template)
+ *
+ * @author Joseph Josue Forestal
+ */
+public class SimulationPage {
+    private SubScene subSceneSimulation;
+    private Button buttonAdd;
+    private HBox hBoxRootNode;
+}
