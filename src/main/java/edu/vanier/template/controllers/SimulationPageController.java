2a9906d74a20417cb3248be683311706b1fb3670
josuevanier <150824779+josuevanier@users.noreply.github.com>
2025-03-15 21:40:50 -0400

    Method intialized and addPlanet

--- src/main/java/edu/vanier/template/controllers/SimulationPageController.java
+++ src/main/java/edu/vanier/template/controllers/SimulationPageController.java
@@ -1,6 +1,11 @@
 package edu.vanier.template.controllers;
 
 import javafx.fxml.FXML;
+import javafx.scene.SubScene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+
+import java.awt.*;
 
 /**
  * Fxml controller for simulation page
@@ -8,5 +13,37 @@ import javafx.fxml.FXML;
  * @author Joseph Josue Forestal
  */
 public class SimulationPageController {
+    private String stringTemplateName;
+    @FXML
+    StackPane stackPaneRootNode;
+    @FXML
+    SubScene subSceneSimulationPage;
+    @FXML
+    Button buttonAddPlanet;
+    @FXML
+    public void initialize(){
+        // handles the logic of the add button
+        buttonAddPlanet.setOnAction(e->{});
+    }
 
+    public  void addPlanet(){
+        //add planet logic, this is probably going to a switch scene customization
+    }
+    public  SimulationPageController(String stringTemplateName){
+        this.stringTemplateName = stringTemplateName;
+    }
+
+
+
+    public void loadAssets(){
+        if (this.stringTemplateName.equals( null) ||(this.stringTemplateName.isEmpty() || this.stringTemplateName.isBlank())){return;}
+        if(this.stringTemplateName.equals("")){
+            //Add logic
+        } else if (this.stringTemplateName.equals("")) {
+            //add logic
+        }
+        else {
+//add logic
+        }
+    }
 }
