c80fa00fd2c19c7897440365c9e2c8f60efe6f7f
josuevanier <150824779+josuevanier@users.noreply.github.com>
2025-03-21 13:06:41 -0400

    Binding property set , still has some bug but it runs fine

--- src/main/java/edu/vanier/template/controllers/SimulationMainPageController.java
+++ src/main/java/edu/vanier/template/controllers/SimulationMainPageController.java
@@ -1,5 +1,6 @@
 package edu.vanier.template.controllers;
 
+import javafx.animation.AnimationTimer;
 import javafx.fxml.FXML;
 import javafx.scene.*;
 import javafx.scene.control.Button;
@@ -7,6 +8,8 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TitledPane;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
+import javafx.scene.paint.PhongMaterial;
+import javafx.scene.shape.Sphere;
 
 public class SimulationMainPageController {
     // All the main containers:
@@ -29,6 +32,7 @@ public class SimulationMainPageController {
         // Components:
         @FXML
         private AnchorPane anchorPaneToolBarKit;
+        @FXML
         private TitledPane tiltPanePlanets;
         @FXML
         private TilePane tilePanePlanets;
@@ -67,20 +71,74 @@ public class SimulationMainPageController {
     private Camera camera = new PerspectiveCamera();
 
 
+    public  void handlerButtonAddPlanetEvent(){
+        if (!vboxAddPlanetButton.isVisible()){return;}
+        // animations can be added here
+        hboxRootToolBar.setVisible(true);
+        vboxAddPlanetButton.setVisible(false);
+        vboxAddPlanetButton.setManaged(false);
+        this.tiltPanePlanets.setExpanded(true);
+
+    }
+    public void handlerTitlePaneEvent(){
+
+        this.tiltPanePlanets.expandedProperty().addListener((obs,oldValue,newValue)->{
+
+            if(!newValue){
+                tiltPanePlanets.setExpanded(false);
+
+                hboxRootToolBar.setVisible(false);
+                hboxRootToolBar.setManaged(false);
+                vboxAddPlanetButton.setVisible(true);
+                vboxAddPlanetButton.setManaged(true);
+            }
+        });
+
+    }
+
     public  void  initializeBinding(){
+        this.tiltPanePlanets.setExpanded(false);
+        Sphere sphere = new Sphere(30);
+        sphere.setMaterial(new PhongMaterial(Color.CORAL));
+        this.tilePanePlanets.getChildren().add(sphere);
         if(this.subSceneSimulation == null || this.vboxMainRootNode == null){return;}
+
         vboxMainRootNode.sceneProperty().addListener((obs, oldScene, newScene)->{
+            this.vboxMainRootNode.prefWidthProperty().bind(newScene == null ? oldScene.widthProperty() : newScene.widthProperty());
+            this.vboxMainRootNode.prefHeightProperty().bind(newScene == null ? oldScene.heightProperty(): newScene.heightProperty());
+
+            this.anchorPaneMainRootNode.prefWidthProperty().bind(newScene == null ? oldScene.widthProperty() : newScene.widthProperty());
+            this.anchorPaneMainRootNode.prefHeightProperty().bind(newScene == null ? oldScene.heightProperty(): newScene.heightProperty());
+
             this.subSceneSimulation.widthProperty().bind(newScene == null ? oldScene.widthProperty() : newScene.widthProperty());
             this.subSceneSimulation.heightProperty().bind(newScene == null ? oldScene.heightProperty(): newScene.heightProperty());
-            this.anchorPaneMainRootNode.setPrefWidth(newScene == null ? oldScene.getWidth() :  newScene.getWidth());
-            this.anchorPaneMainRootNode.setPrefHeight(newScene == null ? oldScene.getHeight() : newScene.getHeight());
-            Scene scene = vboxMainRootNode.getScene();
-           if (newScene != null) {
-               vboxAddPlanetButton.layoutXProperty().bind(scene.widthProperty().subtract(170));
 
-               vboxAddPlanetButton.layoutYProperty().bind(scene.heightProperty().subtract(180));
-           }
+            this.hboxRootToolBar.prefWidthProperty().bind(newScene == null ? oldScene.widthProperty() : newScene.widthProperty());
+           // this.hboxRootToolBar.prefHeightProperty().bind(newScene == null ? oldScene.heightProperty(): newScene.heightProperty());
 
+           // this.hboxRootToolBar.setStyle("-fx-border-color: red");
+            this.anchorPaneToolBarKit.prefWidthProperty().bind((newScene == null ? oldScene.widthProperty() : newScene.widthProperty()));
+           // this.anchorPaneToolBarKit.setStyle("-fx-border-color: blue");
+
+            this.tiltPanePlanets.prefWidthProperty().bind(hboxRootToolBar.prefWidthProperty());
+            this.tilePanePlanets.prefWidthProperty().bind((hboxRootToolBar.prefWidthProperty()));
+            //this.hboxRootToolBar.layoutYProperty().bind(newScene == null ? oldScene.heightProperty().subtract(hboxRootToolBar.getHeight()): newScene.heightProperty().subtract(hboxRootToolBar.getHeight()));
+
+
+
+
+
+
+        });
+
+        vboxMainRootNode.widthProperty().addListener((obs,oldWidth,newWidth)->{
+            vboxAddPlanetButton.setLayoutX(newWidth.doubleValue() - 150);
+
+
+        });
+        vboxMainRootNode.heightProperty().addListener((obs,oldHeight,newHeight)->{
+            vboxAddPlanetButton.setLayoutY(newHeight.doubleValue() - 150);
+            hboxRootToolBar.setLayoutY(newHeight.doubleValue() * 0.88);
         });
     }
 
@@ -91,8 +149,12 @@ public class SimulationMainPageController {
     }
     @FXML
     public  void initialize(){
+
+        buttonAddPlanet.setOnAction(e->{handlerButtonAddPlanetEvent();});
         initializeBinding();
+
         setSubSceneSimulation();
+        handlerTitlePaneEvent();
     }
 
 }
