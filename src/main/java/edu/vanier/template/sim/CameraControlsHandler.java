fa08603dac63eb276116aadd0adf40b18701b2af
josuevanier <150824779+josuevanier@users.noreply.github.com>
2025-03-21 20:02:53 -0400

    Camera controls

---
+++ src/main/java/edu/vanier/template/sim/CameraControlsHandler.java
@@ -0,0 +1,8 @@
+package edu.vanier.template.sim;
+
+import javafx.scene.Camera;
+import javafx.scene.PerspectiveCamera;
+
+public class CameraControlsHandler {
+    private final Camera camera  = new PerspectiveCamera(true);
+}
