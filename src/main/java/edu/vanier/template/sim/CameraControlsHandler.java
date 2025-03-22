4d57e86ded7c3283071c64f48197413d1260916f
josuevanier <150824779+josuevanier@users.noreply.github.com>
2025-03-21 20:08:04 -0400

    Camera controls added proper data members

--- src/main/java/edu/vanier/template/sim/CameraControlsHandler.java
+++ src/main/java/edu/vanier/template/sim/CameraControlsHandler.java
@@ -2,7 +2,30 @@ package edu.vanier.template.sim;
 
 import javafx.scene.Camera;
 import javafx.scene.PerspectiveCamera;
+import javafx.scene.input.KeyCode;
+
+import java.util.HashSet;
+import java.util.Set;
 
 public class CameraControlsHandler {
     private final Camera camera  = new PerspectiveCamera(true);
+
+
+    //This is for rotations yaw is about the Y axis (left or right) and pitch is a rotation about the X axis (up and down)
+    private double yaw = 0;
+    private double pitch = 0;
+
+    private final double MAX_SPEED = 0.3;
+
+    private double speed = 0;
+
+
+    private  double acceleration = 0.02;
+
+    private  boolean  rightClickedHeld = false;
+
+    private  double lastMouseX, lastMouseY;
+
+
+    private  final Set<KeyCode> activeKeys = new HashSet<>();
 }
