bde0c98cf1f1fad0e4ee6d24d5e36eaf92ffbce0
josuevanier <150824779+josuevanier@users.noreply.github.com>
2025-03-21 23:12:04 -0400

    trig added now the key inputs are the next step

--- src/main/java/edu/vanier/template/sim/CameraControlsHandler.java
+++ src/main/java/edu/vanier/template/sim/CameraControlsHandler.java
@@ -1,5 +1,6 @@
 package edu.vanier.template.sim;
 
+import edu.vanier.template.math.Vector3D;
 import javafx.scene.Camera;
 import javafx.scene.PerspectiveCamera;
 import javafx.scene.input.KeyCode;
@@ -9,7 +10,11 @@ import javafx.scene.transform.Transform;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.Vector;
 
+/**
+ * @author Josue
+ */
 public class CameraControlsHandler {
     private final Camera camera  = new PerspectiveCamera(true);
 
@@ -50,6 +55,8 @@ public class CameraControlsHandler {
 
         pitch = Math.max(-89, Math.min(89, pitch) );
 
+        updateCameraRotation();
+
         lastMouseX = event.getSceneX();
         lastMouseY = event.getSceneY();
     }
@@ -60,7 +67,7 @@ public class CameraControlsHandler {
 
     public  void updateCameraRotation(){
         // create concateneation is just adding multiple rotations transform in one transform
-        //because if we add i in one goe for some reason it did not work
+        //because if we add in one goe for some reason it did not work
         Transform rotation = new Rotate(yaw,Rotate.Y_AXIS)
                 .createConcatenation(new Rotate(pitch, Rotate.X_AXIS));
 
@@ -68,6 +75,27 @@ public class CameraControlsHandler {
     }
 
     private void  updateMovement(){
+        double radYaw = Math.toRadians(yaw);
+        double radPitch = Math.toRadians(pitch);
+
+        // this step is crucial since forward is not always forward so we have to get a relative direction
+        // just like in roblox where you cant get a look vector, to do that we use our yaw and pitch values
+        /*
+        Keep in mind javafx'axis are inverted !
+         Understand did the trig:
+         Math.sin(radYaw) : what it does is essentially take the x and z forward direction
+         Math.pitch(radPitch): take
+         */
+        Vector3D vector3DForward = new Vector3D(Math.sin(radYaw) * Math.cos(radPitch),-Math.sin(radPitch), Math.cos(radYaw) * Math.cos(radPitch));
+
+        Vector3D vector3DRight= new Vector3D(Math.cos(radYaw), 0,-Math.sin(radYaw));
+
+        Vector3D vector3DMoveVector = new Vector3D(0,0,0);
+
+
+
+
+
 
     }
 }
